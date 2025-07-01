649256463914dda74f5c8e0e792fa9a1c77431e8
harry5556 <haharry555@proton.me>
2025-07-01 17:46:10 +0800

    add watchlist method

--- javascript/src/index.ts
+++ javascript/src/index.ts
@@ -35,7 +35,7 @@ export interface DexAggregatorOptions {
   streamUrl?: string;
 }
 
-export const LIB_VERSION = "0.0.51";
+export const LIB_VERSION = "0.0.52";
 
 class UserAgentMiddleware implements Middleware {
   public pre(context: RequestContext): Promise<RequestContext> {
