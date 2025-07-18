494a3e4f31d6b3239483ed91f49deb27b231724a
harry5556 <haharry555@proton.me>
2025-07-18 22:39:00 +0800

    update

--- javascript/src/index.ts
+++ javascript/src/index.ts
@@ -36,7 +36,7 @@ export interface DexAggregatorOptions {
   streamUrl?: string;
 }
 
-export const LIB_VERSION = "0.0.76";
+export const LIB_VERSION = "0.0.80";
 
 class UserAgentMiddleware implements Middleware {
   public pre(context: RequestContext): Promise<RequestContext> {
