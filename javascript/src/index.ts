02fcf5146c069fe76920c4141e788ec73b0a30ce
harry5556 <haharry555@proton.me>
2025-08-04 21:37:24 +0800

    add controller version

--- javascript/src/index.ts
+++ javascript/src/index.ts
@@ -37,7 +37,7 @@ export interface DexAggregatorOptions {
   streamUrl?: string;
 }
 
-export const LIB_VERSION = "0.0.87";
+export const LIB_VERSION = "0.0.88";
 
 class UserAgentMiddleware implements Middleware {
   public pre(context: RequestContext): Promise<RequestContext> {
