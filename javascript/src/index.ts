ccd4509520572c6f5f3c78dc4eac12cb53b6f5c1
harry5556 <haharry555@proton.me>
2025-08-31 04:05:58 +0800

    add completionRatio field

--- javascript/src/index.ts
+++ javascript/src/index.ts
@@ -37,7 +37,7 @@ export interface DexAggregatorOptions {
   streamUrl?: string;
 }
 
-export const LIB_VERSION = "0.0.100";
+export const LIB_VERSION = "0.0.101";
 
 class UserAgentMiddleware implements Middleware {
   public pre(context: RequestContext): Promise<RequestContext> {
